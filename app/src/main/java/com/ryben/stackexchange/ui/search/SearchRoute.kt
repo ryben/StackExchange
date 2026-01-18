fc005796fc0972583c61cabf264ba6e043cf41dd
rey <reybenedicto06@gmail.com>
2026-01-18 16:33:28 +0800

    Handle Idle, Loading, Empty state

--- app/src/main/java/com/ryben/stackexchange/ui/search/SearchRoute.kt
+++ app/src/main/java/com/ryben/stackexchange/ui/search/SearchRoute.kt
@@ -1,22 +1,25 @@
 package com.ryben.stackexchange.ui.search
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -26,40 +29,39 @@ import com.ryben.stackexchange.domain.model.User
 @Preview
 @Composable
 fun SearchScreenPreview() {
-    var searchText by mutableStateOf("")
-
-    SearchScreen(
-        searchText = searchText,
-        onSearchTextChanged = {},
-        onSearch = {},
+    val uiState = SearchUiState(
         searchResults = listOf(
             User("0", "Michael Scott", "Scranton", "", ""),
             User("1", "Jim Halpert", "Scranton", "", ""),
             User("2", "Dwight Schrute", "Scranton", "", ""),
-        )
+        ),
+    )
+
+    SearchScreen(
+        uiState = uiState,
+        onSearchTextChanged = {},
+        onSearch = {},
     )
 }
 
 
 @Composable
 fun SearchRoute(viewModel: SearchViewModel = hiltViewModel()) {
-    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val uiState by viewModel.searchUiState.collectAsStateWithLifecycle()
 
     SearchScreen(
-        searchText = uiState.searchText,
+        uiState = uiState,
         onSearchTextChanged = viewModel::onSearchTextUpdated,
         onSearch = viewModel::onSearch,
-        searchResults = uiState.searchResults
     )
 }
 
 
 @Composable
 fun SearchScreen(
-    searchText: String,
+    uiState: SearchUiState,
     onSearchTextChanged: (newSearchText: String) -> Unit,
     onSearch: (searchText: String) -> Unit,
-    searchResults: List<User>
 ) {
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         Column(
@@ -70,22 +72,52 @@ fun SearchScreen(
             // Search bar
             Row(modifier = Modifier.fillMaxWidth()) {
                 TextField(
-                    value = searchText,
+                    value = uiState.searchText,
                     onValueChange = { onSearchTextChanged(it) },
                     singleLine = true,
                 )
-                Button(onClick = { onSearch(searchText) }) {
+                Button(onClick = { onSearch(uiState.searchText) }) {
                     Text("Search")
                 }
             }
 
             Text(text = "Search results")
 
-            // Search Results List
-            LazyColumn(modifier = Modifier.fillMaxWidth()) {
-                items(items = searchResults) { item ->
-                    Text(text = item.name, fontSize = 18.sp)
+            Box {
+                when (uiState.status) {
+
+                    SearchStatus.IDLE -> {
+                        Text(text = "Results will show here")
+                    }
+
+                    SearchStatus.LOADING -> {
+                        CircularProgressIndicator(
+                            modifier = Modifier.width(64.dp),
+                            color = MaterialTheme.colorScheme.secondary,
+                            trackColor = MaterialTheme.colorScheme.surfaceVariant,
+                        )
+                    }
+
+                    SearchStatus.SUCCESS -> {
+                        if (uiState.searchResults.isNotEmpty()) {
+                            // Search Results List
+                            LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                                items(items = uiState.searchResults, key = { it.id }) { item ->
+                                    Text(text = item.name, fontSize = 18.sp)
+                                }
+                            }
+                        } else {
+                            Text(text = "No matches")
+
+                        }
+                    }
+
+                    SearchStatus.ERROR -> {
+                        // TODO: Show error message
+                    }
                 }
+
+
             }
         }
     }
--- app/src/main/java/com/ryben/stackexchange/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/ryben/stackexchange/ui/search/SearchViewModel.kt
@@ -10,32 +10,49 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
 
 
-data class UiState(
-    val searchText: String,
-    val searchResults: List<User>,
+data class SearchUiState(
+    val searchText: String = "",
+    val searchResults: List<User> = emptyList(),
+    val status: SearchStatus = SearchStatus.IDLE,
 )
+
+enum class SearchStatus {
+    IDLE, LOADING, SUCCESS, ERROR
+}
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(private val userRepository: UserRepository) :
     ViewModel() {
-    private val _uiState = MutableStateFlow(UiState("", emptyList()))
-    val uiState: StateFlow<UiState> = _uiState.asStateFlow()
+    private val _searchUiState = MutableStateFlow(SearchUiState())
+    val searchUiState: StateFlow<SearchUiState> = _searchUiState.asStateFlow()
 
     fun onSearchTextUpdated(newSearch: String) {
-        _uiState.update { it.copy(searchText = newSearch) }
+        _searchUiState.update { it.copy(searchText = newSearch) }
     }
 
     fun onSearch(searchText: String) {
+        // Ignore if empty search
+        if (searchText.isBlank()) {
+            return
+        }
+
         viewModelScope.launch {
+            _searchUiState.update { it.copy(status = SearchStatus.LOADING) }
+
             userRepository.searchUserByName(searchText)
                 .onSuccess { result ->
-                    _uiState.update { it.copy(searchResults = result) }
+                    _searchUiState.update {
+                        it.copy(
+                            status = SearchStatus.SUCCESS,
+                            searchResults = result
+                        )
+                    }
                 }
                 .onFailure {
+                    _searchUiState.update { it.copy(status = SearchStatus.ERROR) }
                     // TODO: Show error in UI
                 }
         }
